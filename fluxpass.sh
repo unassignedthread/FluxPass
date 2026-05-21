f2548fc7ba7e952e8e06808d23957e3d0bd3c8d4
Vladislav Gurolev <unassignedthread@gmail.com>
2026-05-21 11:33:38 +0300

    Fix launch script for system install

--- fluxpass.sh
+++ fluxpass.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 export _JAVA_AWT_WM_NONREPARENTING=1
+
+if [ -f /usr/share/java/fluxpass/fluxpass.jar ]; then
+    exec java -jar /usr/share/java/fluxpass/fluxpass.jar "$@"
+fi
+
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 JAR="$SCRIPT_DIR/target/fluxpass.jar"
 
